d80d28922986b8fa9f2c738646a378e3bc0346de
Koji5 <sakamoto5002@gmail.com>
2025-10-15 16:01:52 +0900

    ci: fix

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -2,6 +2,6 @@
 Rails.application.configure do
   # ビルド定義（既にあればそのままでOK）
   config.dartsass.builds = {
-    "app/assets/stylesheets/application.scss" => "app/assets/builds/application.css"
+    "application.scss" => "app/assets/builds/application.css"
   }
 end
