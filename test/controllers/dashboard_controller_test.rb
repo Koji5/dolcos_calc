60e110476393e6b16317ead4568c61a77990509f
Koji5 <sakamoto5002@gmail.com>
2025-10-20 11:40:28 +0900

    ci:

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  setup do
+    @user = User.create!(
+      email: "u@example.com",
+      password: "Passw0rd!",
+      password_confirmation: "Passw0rd!",
+      confirmed_at: Time.current  # ← confirmable 対策（これが重要）
+    )
+  end
+
+  test "should get show when signed in" do
+    sign_in @user            # ← ここでログイン
     get dashboard_show_url
     assert_response :success
   end
 end
+
